517ec1dd6450b4ed65c8fbbccf01f8fdc1bac008
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-22 13:31:50 -0500

    changes

--- src/components/chat/ChatList.js
+++ src/components/chat/ChatList.js
@@ -1,1 +1,13 @@
 import React, { Component } from "react"
+
+export default class ChatList extends Component {
+    render(){
+        return(
+            <React.Fragment>
+                <section className = "chat">
+                
+                </section>
+            </React.Fragment>
+        )
+    }
+}
--- src/components/chat/ChatManager.js
+++ src/components/chat/ChatManager.js
@@ -1,3 +1,5 @@
+const url = "http://localhost:8088"
+
 export default {
     get(id) {
         return fetch(`${url}/messages/${id}`).then(l => l.json())
