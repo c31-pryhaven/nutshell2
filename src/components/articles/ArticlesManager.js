b990316d38f8e19734a69b013d71cb081bbb2c66
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-04-22 11:00:55 -0500

    Creating folders and files

---
+++ src/components/articles/ArticlesManager.js
@@ -0,0 +1,26 @@
+const url = "http://localhost:8088"
+
+
+export default {
+    get(id) {
+        return fetch(`${url}/articles/${id}`).then(l => l.json())
+    },
+    getAll() {
+        return fetch(`${url}/articles`).then(l => l.json())
+    },
+    delete(id) {
+        return fetch(`${url}/articles/${id}`, {
+            method: "DELETE"
+        })
+        .then(l => l.json())
+    },
+    postArticle(newArticle) {
+        return fetch(`${url}/articles`, {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newArticle)
+        }).then(data => data.json())
+    }
+}
