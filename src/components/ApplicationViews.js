8db40f8c652b6f757d52c9a290ec2094a0a802cc
anneraev <47757104+anneraev@users.noreply.github.com>
2019-04-24 14:18:21 -0500

    styling

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -166,7 +166,7 @@ class ApplicationViews extends Component {
         />
         <Route
           path="/messages" render={props => {
-            return <ChatList messages={this.state.messages} addMessage={this.addMessage} users={this.state.users} />
+            return <ChatList {...props} messages={this.state.messages} addMessage={this.addMessage} users={this.state.users} />
           }}
         />
         <Route
