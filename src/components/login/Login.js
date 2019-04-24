b3b223400f1f987ec674f48ec10cba278677f4d3
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-04-24 14:28:19 -0500

    using session storage for articles

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react"
+import UserManager from "../users/UserManager"
 
 
 export default class Login extends Component {
 
     state = {
-        email: "",
-        password: ""
+        userName: "",
+        email: ""
     }
 
     handleFieldChange = (evt) => {
@@ -13,40 +14,52 @@ export default class Login extends Component {
         stateToChange[evt.target.id] = evt.target.value
         this.setState(stateToChange)
     }
-
     handleLogin = (e) => {
         e.preventDefault()
+        UserManager.getAll()
+        .then(userList => {
+            let tempUserName = userList.find(element => element.userName.toLowerCase() ===
+            this.state.userName.toLowerCase() && element.email.toLowerCase() ===
+            this.state.email.toLowerCase())
+            if (tempUserName) {
+                sessionStorage.setItem("userId", tempUserName.id)
+                this.props.onLogin()
+                this.props.history.push("/articles") 
+            } else {
+                window.alert("Not Found!")
+        }})
 
-        sessionStorage.setItem(
-            "credentials",
-            JSON.stringify({
-                email: this.state.email,
-                password: this.state.password
-            })
-        )
     }
 
     render() {
         return (
             <form onSubmit={this.handleLogin} className="content">
                 <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                <label htmlFor="inputEmail">
-                    Email address
+                <label htmlFor="userName">
+                    User Name
+                </label>
+                <input onChange={this.handleFieldChange} type="userName"
+                    id="userName"
+                    placeholder="user Name"
+                    required="" autoFocus="" />
+                <label htmlFor="email">
+                    Email
                 </label>
                 <input onChange={this.handleFieldChange} type="email"
                     id="email"
-                    placeholder="Email address"
-                    required="" autoFocus="" />
-                <label htmlFor="inputPassword">
-                    Password
-                </label>
-                <input onChange={this.handleFieldChange} type="password"
-                    id="password"
-                    placeholder="Password"
+                    placeholder="email"
                     required="" />
-                <button type="submit">
+                <button type="submit"
+                onClick={() => 
+                                this.handleLogin
+                            }>
                     Sign in
                 </button>
+                <button type="Register"
+                onClick={() => {
+                                this.props.history.push("/login/new")
+                            }}>
+                Register</button>
             </form>
         )
     }
